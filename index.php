9c9935a0283ffd5e6a155dbe95d7f033eb170d9b
keisuke-watanabe <k-watanabe@dji.co.jp>
2017-03-24 21:09:59 +0900

    update

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
-<!doctype html>
-<html dir="ltr" lang="ja">
-<head>
-<meta charset="utf-8">
-<title>テスト</title>
-</head>
+<?php
+//setup
+    $page = "top";
+    require_once('common/php/setup.php');
+    $title = "テンプレ";
+    $discription ="";
+    $keywords ="";
+    $canonical ="";
+    $riottag = $targetdir.'top/js/tags/top.tag';
+?>
+    <?php
+//header
+    include ("$head");
+?>
+        </head>
 
-<body>
+        <body id="<?php echo $page; ?>">
+            <?php
+//header
+    include ("$header");
+?>
+                <div id="contents">
 
-<p>テストマスターから</p>
-</body>
-</html>
+                    <main id="main">
+                    </main>
+
+                    <div class="weather" date="2016/01/01" state="晴れ">
+                    </div>
+                    <riot-tabs></riot-tabs>
+                    <test></test>
+                </div>
+                <?php include ("$footer"); ?>
+                <script>
+                    riot.mount(".weather", "weather")
+                </script>
+                <?php include ("$tag"); ?>
+
+        </body>
+
+        </html>
